b273bc9d3595a799ea7c5345846c861ee1e0ac70
thinktwice13 <marioo.krajacic@gmail.com>
2020-04-24 22:17:09 +0200

    Add expo app

---
+++ app/App.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+const instructions = Platform.select({
+  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Hello world</Text>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+  },
+  titleText: {
+    fontSize: 48,
+    fontWeight: "bold",
+  },
+});
