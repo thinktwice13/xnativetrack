dd431506f0e503c5f8dc9e6048daa6a4a2968399
thinktwice13 <marioo.krajacic@gmail.com>
2020-04-24 16:16:24 +0200

    Add /tracks/new route

--- api/src/routes/trackRoutes.js
+++ api/src/routes/trackRoutes.js
@@ -5,12 +5,28 @@ const Track = mongoose.model("Track");
 
 export default {
   getAll: async (req, res) => {
-    console.log("Here!", { user: req.user });
     try {
       const tracks = await Track.find({ userId: req.user._id });
       res.send(tracks);
     } catch (err) {
       res.send({ err });
+    }
+  },
+  add: async (req, res) => {
+    console.log(req.body);
+
+    const { _id: userId } = req.user;
+    const { name, locations } = req.body; // get this from mobile phone
+
+    if (!name || !locations)
+      return res.status(422).send({ error: "Name and locations not provided" });
+
+    try {
+      const track = new Track({ name, locations, userId });
+      await track.save();
+      res.send("Track saved");
+    } catch (error) {
+      res.send(422).send({ error });
     }
   },
 };
