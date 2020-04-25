3525dc4e4fc2ef9a2c9c20a3a2278af71ab1f900
thinktwice13 <marioo.krajacic@gmail.com>
2020-04-25 16:17:03 +0200

    Add signin/signout reducers

--- app/src/context/authContext.js
+++ app/src/context/authContext.js
@@ -7,8 +7,18 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {};
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signup },
   { isSignedIn: false }
 );
