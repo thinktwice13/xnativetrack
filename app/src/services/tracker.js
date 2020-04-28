ccef08ab7e11168ebbd11479cdaf25890ce46a36
thinktwice13 <marioo.krajacic@gmail.com>
2020-04-28 16:20:53 +0200

    Add linter

--- app/src/services/tracker.js
+++ app/src/services/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ngrokIP } from "../../env_keys";
+import { ngrokIP } from "../env_keys";
 
 export default axios.create({
   baseURL: ngrokIP, // closes in max 8 hours: FIXME: use env
